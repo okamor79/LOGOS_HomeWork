79db5ba257363d6ad80970e48373eade0fa7a283
okamor79 <36225539+okamor79@users.noreply.github.com>
2018-02-09 13:19:49 +0200

    Add home work 2 lesson 4

---
+++ src/JavaCore/HW4_Work2.java
@@ -0,0 +1,30 @@
+package JavaCore;
+
+import java.util.Random;
+
+public class HW4_Work2 {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][8];
+        int max = 0;
+
+        Random random = new Random();
+
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++){
+                arr[i][j] = random.nextInt(200) - 100;
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++){
+                if(arr[i][j] >= max){max = arr[i][j];}
+                System.out.printf("%3d",arr[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Max element in array = " + max);
+
+    }
+}
