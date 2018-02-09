2a896486fc20a7f8ad32914b0341629b2c1b2802
okamor79 <36225539+okamor79@users.noreply.github.com>
2018-02-09 13:39:41 +0200

    Add Home Work Lesson 3

---
+++ src/JavaCore_HomeWork3/HW3_Work1.java
@@ -0,0 +1,33 @@
+package JavaCore_HomeWork3;
+
+public class HW3_Work1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 1; i <= 20; i++){
+            if(i % 2 == 0) {counter += 1;}
+        }
+        int[] myArray = new int[counter];
+        createArray(myArray);
+        printArray(myArray, "R");
+        System.out.println();
+        printArray(myArray,"C");
+    }
+
+    static void createArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = (i + 1) * 2;
+        }
+    }
+
+    static void printArray(int[] arr, String type) {
+        for (int i = 0; i < arr.length; i++) {
+
+            if(type == "C") {
+                System.out.println(arr[i]);
+            } else {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+    }
+}
---
+++ src/JavaCore_HomeWork3/HW3_Work2.java
@@ -0,0 +1,36 @@
+package JavaCore_HomeWork3;
+
+public class HW3_Work2 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            if(i % 2 != 0) count++;
+        }
+        int[] myArray = new int[count];
+        createArray(myArray);
+        printArray(myArray, 0);
+        System.out.println();
+        printArray(myArray, 1);
+    }
+
+    static void createArray(int[] arr) {
+        for (int i = 0, j = 0; i < 100; i++ ){
+            if(i % 2 != 0) {
+                arr[j++] = i;
+            }
+        }
+    }
+
+    static void printArray(int[] arr, int a) {
+        if (a == 0) {
+            for(int i = 0; i < arr.length; i++){
+                System.out.print(arr[i] + " ");
+            }
+        } else if (a == 1) {
+            for(int i = arr.length - 1; i >= 0; i--){
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+    }
+}
