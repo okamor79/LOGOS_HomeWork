62a69ce761625ca30d16a9f43853b80b96abb024
okamor79 <36225539+okamor79@users.noreply.github.com>
2018-02-09 14:11:35 +0200

    Add Home Work Lesson 2

---
+++ src/JavaCore_HomeWork2/HW2_Work5.java
@@ -0,0 +1,13 @@
+package JavaCore_HomeWork2;
+
+public class HW2_Work5 {
+    public static void main(String[] args) {
+        int counter = 0;
+        int start_ = 1;
+        do {
+            counter++;
+            System.out.print(start_ + " ");
+            start_+=2;
+        } while(counter<55);
+    }
+}
---
+++ src/JavaCore_HomeWork2/HW2_Work6.java
@@ -0,0 +1,13 @@
+package JavaCore_HomeWork2;
+
+public class HW2_Work6 {
+    public static void main(String[] args) {
+
+        int i = 90;
+        do {
+            System.out.println(i);
+            i -= 5;
+        } while (i == Math.abs(i));
+
+    }
+}
