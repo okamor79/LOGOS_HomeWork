05857de07c04cb56f81225ff26066c161254404a
okamor79 <36225539+okamor79@users.noreply.github.com>
2018-02-09 13:16:05 +0200

    Add home work 1 lesson 4

---
+++ src/JavaCore/HW4_Work1.java
@@ -0,0 +1,25 @@
+package JavaCore;
+
+import java.util.Random;
+
+public class HW4_Work1 {
+    public static void main(String[] args) {
+        int[][] arr = new int[8][5];
+
+        Random random = new Random();
+
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++){
+                arr[i][j] = random.nextInt(89) + 10;
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
