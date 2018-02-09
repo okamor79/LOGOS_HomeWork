1e7eae9edfa3abfee9175e4b6647ab6e59aef19f
okamor79 <36225539+okamor79@users.noreply.github.com>
2018-02-09 13:25:07 +0200

    Add home work 4 lesson 4

---
+++ src/JavaCore/HW4_Work4.java
@@ -0,0 +1,33 @@
+package JavaCore;
+
+import java.util.Scanner;
+
+public class HW4_Work4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введіть ціле число A  ");
+            if(sc.hasNextInt()) {
+                int a = sc.nextInt();
+                while (true) {
+                    System.out.print("Введіть ціле число B  ");
+                    if(sc.hasNextInt()) {
+                        int b = sc.nextInt();
+                        System.out.println("A + B = " + (int)(a + b));
+                        break;
+                    } else {
+                        String b = sc.next();
+                        System.out.println("Введено не ціле число, повторіть ввід");
+                        System.out.println();
+                    }
+                }
+                break;
+            } else {
+                String a = sc.next();
+                System.out.println("Введено не ціле число, повторіть ввід");
+                System.out.println();
+            }
+        }
+
+    }
+}
