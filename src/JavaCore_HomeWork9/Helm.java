c67f7bf411351f1b76370ae7179d7566979b00c6
okamor79 <okamor@gmail.com>
2018-02-23 01:30:46 +0200

    Home Work

--- src/JavaCore_HomeWork9/Helm.java
+++ src/JavaCore_HomeWork9/Helm.java
@@ -1,4 +1,25 @@
 package JavaCore_HomeWork9;
 
 public class Helm {
+
+    private String mathrial;
+
+    public Helm(String mathrial) {
+        this.mathrial = mathrial;
+    }
+
+    public String getMathrial() {
+        return mathrial;
+    }
+
+    public void setMathrial(String mathrial) {
+        this.mathrial = mathrial;
+    }
+
+    @Override
+    public String toString() {
+        return "Helm{" +
+                "mathrial='" + mathrial + '\'' +
+                '}';
+    }
 }
