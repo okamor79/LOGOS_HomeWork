1e0a351807e3477f54ba3e2b8d99f6ce90f79b0e
okamor79 <okamor@gmail.com>
2018-02-23 01:44:30 +0200

    Home Work

--- src/JavaCore_HomeWork9/Car.java
+++ src/JavaCore_HomeWork9/Car.java
@@ -10,7 +10,66 @@ public class Car {
     private Helm helm;
     private Wheel wheel;
 
+    public Car(String color, String model, int maxSpeed, Body body, Helm helm, Wheel wheel) {
+        this.color = color;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.body = body;
+        this.helm = helm;
+        this.wheel = wheel;
+    }
 
+    public Car(Body body, Helm helm, Wheel wheel) {
+        this.body = body;
+        this.helm = helm;
+        this.wheel = wheel;
+    }
 
+    public String getColor() {
+        return color;
+    }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public Helm getHelm() {
+        return helm;
+    }
+
+    public Wheel getWheel() {
+        return wheel;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", body=" + body +
+                ", helm=" + helm +
+                ", wheel=" + wheel +
+                '}';
+    }
 }
--- src/JavaCore_HomeWork9/Main.java
+++ src/JavaCore_HomeWork9/Main.java
@@ -3,5 +3,13 @@ package JavaCore_HomeWork9;
 public class Main {
     public static void main(String[] args) {
 
+        Car car = new Car(new Body("Sedan"), new Helm("Wood"), new Wheel(15,"Nokian"));
+        car.setColor("Red");
+        car.setModel("Porsche");
+        car.setMaxSpeed(280);
+        System.out.println(car);
+
+
+
     }
 }
