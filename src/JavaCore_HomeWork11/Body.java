903f04cc396abf3fe8b0b364ce71911860d7d88d
okamor79 <okamor@gmail.com>
2018-02-25 20:19:56 +0200

    Home Work

---
+++ src/JavaCore_HomeWork11/Body.java
@@ -0,0 +1,25 @@
+package JavaCore_HomeWork11;
+
+public class Body {
+
+    private String type;
+
+    public Body(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Body{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
---
+++ src/JavaCore_HomeWork11/Main.java
@@ -0,0 +1,27 @@
+package JavaCore_HomeWork11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Car> car = new ArrayList<>();
+
+        car.add(new Car("red", "sedan", 200, new Body("Sedan"), new Helm("Wood"), new Wheel(15, "Nokian")));
+        car.add(new Car("red", "sedan", 200, new Body("Sedan"), new Helm("Wood"), new Wheel(15, "Nokian")));
+        car.add(new Car("red", "sedan", 200, new Body("Sedan"), new Helm("Wood"), new Wheel(15, "Nokian")));
+        car.add(new Car("red", "sedan", 200, new Body("Sedan"), new Helm("Wood"), new Wheel(15, "Nokian")));
+        car.add(new Car("red", "sedan", 200, new Body("Sedan"), new Helm("Wood"), new Wheel(15, "Nokian")));
+        car.add(new Car("red", "sedan", 200, new Body("Sedan"), new Helm("Wood"), new Wheel(15, "Nokian")));
+
+
+//        Car car = new Car(new Body("Sedan"), new Helm("Wood"), new Wheel(15,"Nokian"));
+//        car.setColor("Red");
+//        car.setModel("Porsche");
+//        car.setMaxSpeed(280);
+        System.out.println(car);
+
+
+    }
+}
