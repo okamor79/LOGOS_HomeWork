0b5d5f75d6fc85dc54944fafa860768179503d7d
okamor79 <okamor@gmail.com>
2018-02-28 03:15:08 +0200

    vx

--- src/JavaCore_HomeWork12/Main.java
+++ src/JavaCore_HomeWork12/Main.java
@@ -1,46 +1,31 @@
 package JavaCore_HomeWork12;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
         List<Student> univer = new ArrayList<>();
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
-        univer.add(new Student("Oleg", "Kaminskyi", 15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
+        univer.add(new Student("Oleg", "Kaminskyi", random(7)+15, Student.MALE, 1, "KI"));
 
 
-        Iterator<Student> iter = univer.iterator();
 
-        for (Iterator<> iter)
 
-        System.out.println(univer);
+
+
     }
 
 
