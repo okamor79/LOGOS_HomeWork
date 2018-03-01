9f1e6094f5bea93eb797326bbdf5363d23de1358
okamor79 <okamor@gmail.com>
2018-03-01 21:39:10 +0200

    vx

---
+++ src/JavaCore_HomeWork13/User.java
@@ -0,0 +1,66 @@
+package JavaCore_HomeWork13;
+
+import java.util.Objects;
+
+public class User {
+
+    private String username;
+    private String nickname;
+    private int age;
+
+    public User() {}
+
+    public User(String username, String nickname, int age) {
+        this.username = username;
+        this.nickname = nickname;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(username, age);
+    }
+}
