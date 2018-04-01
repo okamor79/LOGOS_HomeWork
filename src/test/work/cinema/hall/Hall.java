e4565721cb5e5d46a980059bb67aced459ee1990
Oleh R. Kaminskyi <okamor@gmail.com>
2018-04-01 15:34:22 +0300

    Test work cimena Modified hall.java class

--- src/test/work/cinema/hall/Hall.java
+++ src/test/work/cinema/hall/Hall.java
@@ -1,8 +1,7 @@
 package test.work.cinema.hall;
 
 import java.io.Serializable;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Hall implements Serializable {
 
@@ -14,6 +13,8 @@ public class Hall implements Serializable {
     private String addressHall;
     private int capacityHall;
 
+    private List<Hall> hall = new ArrayList<>();
+
     public Hall() {
     }
 
@@ -22,6 +23,49 @@ public class Hall implements Serializable {
         this.nameHall = nameHall;
         this.addressHall = addressHall;
         this.capacityHall = capacityHall;
+    }
+
+    public int getIdHall() {
+        return idHall;
+    }
+
+    public String getNameHall() {
+        return nameHall;
+    }
+
+    public String getAddressHall() {
+        return addressHall;
+    }
+
+    public int getCapacityHall() {
+        return capacityHall;
+    }
+
+    @Override
+    public String toString() {
+        return "Hall{" +
+                "idHall=" + idHall +
+                ", nameHall='" + nameHall + '\'' +
+                ", addressHall='" + addressHall + '\'' +
+                ", capacityHall=" + capacityHall +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hall hall = (Hall) o;
+        return idHall == hall.idHall &&
+                capacityHall == hall.capacityHall &&
+                Objects.equals(nameHall, hall.nameHall) &&
+                Objects.equals(addressHall, hall.addressHall);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(idHall, nameHall, addressHall, capacityHall);
     }
 
     public void start() {
